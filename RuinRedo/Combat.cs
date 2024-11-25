3bace46f8b13cf593abc22ea195af697d2a87a0f
TylerA8 <TPRendering@gmail.com>
2024-11-24 21:58:24 -0600

    Testing begins and initiative tracker

--- RuinRedo/Combat.cs
+++ RuinRedo/Combat.cs
@@ -8,6 +8,19 @@ namespace RuinRedo
 {
     public class Combat
     {
-        public Dictionary<Creature, int> initiativeTracker = new(); 
+        public Dictionary<Creature, int> initiativeTracker = new();
+        Creature[] players;
+        Creature[] enemies;
+
+
+        public Combat(Creature[] players, Creature[] enemies)
+        {
+            this.players = players;
+            this.enemies = enemies;
+        }
+        public Dictionary<Creature, int> Initiative(Creature[] players, Creature[] enemies)
+        {
+            return initiativeTracker;
+        }
     }
 }
